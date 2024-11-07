d9ce409f150d717e90d095c8874f5b2afffbef3e
luiyipopx100x <luistech140616@gmail.com>
2024-11-06 23:22:27 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Programa apps Ya!</title>
+    <link rel="stylesheet" href="estilo1.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <p class="logo">Programa apps Ya!</p>
+            <nav>
+                <a href="#somos-proya">Quienes Somos</a>
+                <a href="#nuestros-programas">Nuestras Apps</a>
+                <a href="#caracteristicas">Características</a>
+            </nav>
+        </div>
+    </header>
+
+    <section id="hero">
+        <h1>Aprendé a Programar Apps <br>con los Mejores</h1>
+        <button>APLICÁ YA!</button>
+    </section>
+
+    <section id="somos-proya">
+        <div class="container">
+            <div class="img-container"></div>
+            <div class="texto">
+
+                <h2>Somos <span class="color-acento">Programa Apps Ya!</span></h2>
+                <p>Aprende a programar de una forma practica y divertida, tenemos a los mejores en el campo de apps, contamos con los mejores horarios y usamos las mejores plataformas como es MIT app inventor.</p>
+            </div>
+        </div>
+    </section>
+
+    <section id="nuestros-programas">
+        <div class="container">
+            <h2>Nuestras Apps</h2>
+            <div class="programas">
+                <div class="carta">
+                    <h3>App Login</h3>
+           <img src="qr-1.png" alt="">
+                    <button>+ Info</button>
+                </div>
+                <div class="carta">
+                    <h3>App Screen</h3>
+                    <img src="qr2.png" alt="">
+                    <button>+ Info</button>
+                </div>
+                <div class="carta">
+                    <h3>App Videojuegos</h3>
+                   <img src="qr-3.png" alt="">
+                   <button>+ Info</button>
+                </div>  
+            </div>
+        </div>
+    </section>
+
+    <section id="caracteristicas">
+        <div class="container">
+            <ul>
+                <li>✔️ 100% online</li>
+                <li>✔️ Flexibilidad de horarios</li>
+                <li>✔️ Soporte 1:1</li>
+                <li>✔️ Asistencia financiera</li>
+            </ul>
+        </div>
+    </section>
+
+    <section id="final">
+        <h2>Listo para armar tu primer app?</h2>
+        <button>APLICÁ YA!</button>
+    </section>
+
+    <footer>
+        <div class="container">
+            <p>&copy; Programa apps Ya 2021</p>
+        </div>
+    </footer>
+</body>
+</html>
